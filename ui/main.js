b1766702913de47be011a06d2c54694b16341417
sumitnptel <30831771+sumitnptel@users.noreply.github.com>
2017-08-31 16:40:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,12 @@ var element = document.getElementById('main-text'
 element.innerHTML = 'new value';
 // moving the image
 var img = document.getElementById('madi');
+var marginLeft= 0;
+function moveRight() {
+    marginLeft=marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick= function(){
-    img.style.marginLeft = '100px';
+    
     
 };
