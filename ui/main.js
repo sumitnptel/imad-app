45112da0f3e10c65da0aeb57cf4b5510e400e539
sumitnptel <30831771+sumitnptel@users.noreply.github.com>
2017-09-03 14:30:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ button.onclick = function () {
     
 };
 // make the request 
-varnameInput=  document.getElementById('name');
+var nameInput=  document.getElementById('name');
 var name=nameInput.value;
 var submit=  document.getElementById('submit_btn');
 submit.onclick = function(){
