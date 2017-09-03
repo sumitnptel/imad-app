c9c3803e7c2c8da4b98e5a4a1e959a21eb2ea740
sumitnptel <30831771+sumitnptel@users.noreply.github.com>
2017-09-03 15:20:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,7 +54,7 @@ submit.onclick = function(){
     
     //make the request
     var nameInput=  document.getElementById('name');
-    var name=nameInput.value;
+    var name = nameInput.value;
     request.open('GET',' http://sumitku45.imad.hasura-app.io/submit-name?name='+ name,true);
     request.send(null);
     
